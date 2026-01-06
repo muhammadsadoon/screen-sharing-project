b3f490cdc7679752a9b7790db643feb623b22b4e
muhammadsadoon <muhammadsadoonsohail786@gmail.com>
2026-01-06 09:16:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import ShareScreen from "./components/ShareScreen";
 import Viewer from "./components/Viewer";
 import "./App.css";
-import {} from "react";
 
 function App() {
-
-  useEffect(()=>{
-    document.title = "Sharing app";
-  },[]);
   return (
     <div className="app-container">
       <header className="app-header">
